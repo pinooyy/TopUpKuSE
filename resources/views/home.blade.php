676e6ef7f482e6adafc95c287b3048135abd1f0c
ankkoyum <152789382+ankkoyum@users.noreply.github.com>
2025-03-05 12:34:43 +0700

    NAV BAR HOME JADI
    yeyeyeye

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,21 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Homepage</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="css/stylo.css">
 
 </head>
 <body>
     <div class="navContainer">
         <nav class="wrapperNav">
             <div class="logo">
-                <img src="{{ asset('Assets SoftEng/TOPUPKU LOGO.png') }}" alt="TopUpKu Logo">
+                <img src='Assets SoftEng/TOPUPKU LOGO.png' alt="TopUpKu Logo">
             </div>
             <ul class="navigationBtn">
                 <li><a href="/" class="active">HOME</a></li>
                 <li><a href="/">PRODUCTS</a></li>
                 <li><a href="{{ route('cart') }}">CART</a></li>
                 <li><a href="/">CONTACT US</a></li>
-                <li><img src="{{ asset('Assets SoftEng/user logo.png') }}" alt="User Logo"></li>
+                <li>
+                    <button class="user-btn">
+                        <i class="fas fa-user" alt="User Logo"></i>
+                    </button>
+                </li>
             </ul>
         </nav>
     </div>
