24b56250305839b9f7f5d005b0cd8ccd48ef4da2
Oreoo13 <nicmatt.aov@gmail.com>
2025-03-13 12:05:05 +0700

    update backend template

--- app/Http/Controllers/Product.php
+++ app/Http/Controllers/Product.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class Product extends Controller
 {
+    public function getAllProducts(){
+        //Ambil semua produk (bisa difilter berdasarkan ID game).
+    }
+
+    
+
+    public function searchProducts() {
+        //Cari produk berdasarkan kata kunci di nama item.
+    }
+
+    public function getProductById(){
+        //Ambil detail produk spesifik berdasarkan ID produk.
+    }
+
     //untuk forward products dari database ke front end, dimana product yang udah ada di database bisa dishow di frontend
 }
--- app/Http/Controllers/UserProfile.php
+++ app/Http/Controllers/UserProfile.php
@@ -1,11 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 
 class UserProfile extends Controller
 {
+    function ChangeUsn(){
+
+    }
+
+    function ChangePw(){
+
+    }
+
+    function ChangeEmail(){
+
+    }
+
+    function Confirm(){
+
+    }
+    
     //ini digunakan untuk alter information yang sudah ada di dalam database user
     //misal mau ganti email, pw atau usn si user
 }
